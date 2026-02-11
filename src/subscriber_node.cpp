7304db0c7dc6b54fafa45d69ea4cd31c93ea0ed0
karthikgonuguntla <karthikgonuguntla777@gmail.com>
2026-02-11 09:07:25 +0530

    Completed ROS2 subscriber node

--- src/subscriber_node.cpp
+++ src/subscriber_node.cpp
@@ -26,7 +26,7 @@ int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<SubscriberNode>());
-  rclcpp::shutdown1();
+  rclcpp::shutdown();
   return 0;
 }
 
